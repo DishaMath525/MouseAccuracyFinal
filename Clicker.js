d35850b76c5c6566e11ce87fcf8be2895f3fdcc2
Josh-cmd298 <jlee1926@westada.org>
2021-05-26 08:49:51 -0600

    add circles

--- Clicker.js
+++ Clicker.js
@@ -3,15 +3,16 @@ var ctx = document.getElementById("canvas").getContext("2d");
 
 
 function run() {
-    
-  drawCircle();
+  setInterval(drawCircle, 500)
+  
 }
 
-function drawCircle(event) {
-    var x =  Math.floor(Math.random() * 600);
-    var y =  100;
-    var radius = 10;
+function drawCircle() {
+    var x =  Math.floor(Math.random() * 580);
+    var y =  Math.floor(Math.random() * 580);
+    var radius = 20;
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    //ctx.fillStyle();
     ctx.fill();
 }
