c838f4e309ee790de575bff6720a1b41abde6a2a
DishaMath525 <dishamath@gmail.com>
2021-05-28 08:43:55 -0600

    added some cool functionality

--- Clicker.js
+++ Clicker.js
@@ -1,18 +1,28 @@
 var canvas = document.getElementById("canvas");
 var ctx = document.getElementById("canvas").getContext("2d");
+var interval;
 
 
 function run() {
-  setInterval(drawCircle, 500)
+  interval = setInterval(drawCircle, 500)
   
 }
 
 function drawCircle() {
-    var x =  Math.floor(Math.random() * 580);
+    var x =  Math.floor(Math.random() * 980);
     var y =  Math.floor(Math.random() * 580);
     var radius = 20;
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     //ctx.fillStyle();
     ctx.fill();
+}
+
+function end(){
+  clearInterval(interval);
+  window.location =  "file:///home/user10/Desktop/MouseAccuracyFinal/end-page.html";
+}
+
+function restart(){
+  window.location = "file:///home/user10/Desktop/MouseAccuracyFinal/Clicker.html";
 }
