0efa44ea3b5b6b4ab4d55aa54a33165167df8d7d
DishaMath525 <dishamath@gmail.com>
2021-06-01 08:44:22 -0600

    wokring on click count

--- Clicker.js
+++ Clicker.js
@@ -1,21 +1,53 @@
 var canvas = document.getElementById("canvas");
 var ctx = document.getElementById("canvas").getContext("2d");
 var interval;
+var count = 0;
+var x;
+var y;
+var cx;
+var cy;
+var radius;
+var circle;
 
 
 function run() {
-  interval = setInterval(drawCircle, 500)
+  interval = setInterval(drawCircle, 500);
+  document.addEventListener("click",countClicks(e));
   
 }
 
 function drawCircle() {
-    var x =  Math.floor(Math.random() * 980);
-    var y =  Math.floor(Math.random() * 580);
-    var radius = 20;
+    cx =  Math.floor(Math.random() * 980);
+    cy =  Math.floor(Math.random() * 580);
+    radius = 40;
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    circle = ctx.arc(cx, cy, radius, 0, 2 * Math.PI);
     //ctx.fillStyle();
     ctx.fill();
+}
+
+
+function countClicks(e){
+  x = e.screenX;
+  y = e.screenY;
+
+    if (pointInCircle(x, y, cx, cy, radius)) {
+        document.addEventListener("click", function(event){
+          count++;
+         
+          document.getElementById("score").innerHTML = "Number Of Clicks: " + count;
+    });
+  }
+}
+function pointInCircle(x, y, cx, cy, radius) {
+ 
+  var dist_points = (x - cx) * (x - cx) + (y - cy) * (y - cy);
+  radius *= radius;
+  if (dist_points < radius) {
+      return true;
+  }
+  return false;
+
 }
 
 function end(){
